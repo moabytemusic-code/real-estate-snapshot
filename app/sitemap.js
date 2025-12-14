91b5101e0db1a00edd1b6c3e708e33684ff9b818
KD5000 <kd5000@KD5000s-MacBook-Pro.local>
2025-12-13 23:37:39 -0500

    Add SEO sitemaps and robots.txt

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://real-estate-snapshot.vercel.app',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+    ]
+}
